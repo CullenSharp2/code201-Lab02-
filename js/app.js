3b32850537a8dd3dbc44ffb029b8bc736d0a6758
SkylerJohnson102020 <basseuph@gmail.com>
2021-03-09 15:45:20 -0800

    Lab02

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// 3 data types
+// let a = 'some text';
+// let b = 4;
+// let c = true;
+// let d = [1,4,2,'some text',4,true,5,false];
+// // above line 5 is an example of an array
+// console.log(typeof(b));
+
+// conditionals
+
+let ageAnswer = prompt('What is my age?');    
+    if (ageAnswer > 37) {
+       alert('Too High');
+    } else if (ageAnswer < 37) {
+       alert('Too Low');
+    } else {
+       alert('Correct');
+}
+
+let birthPlaceAnswer = prompt('True or False: I was born in Montana.').toLowerCase();
+    if (birthPlaceAnswer === 'true') {
+        alert('Yep!');
+    } else { 
+        alert('Nice try, but no!');
+    }
+
+let instrumentAnswer = prompt('True of False: I play a woodwind instrument').toLowerCase();
+    if (instrumentAnswer === 'false') {
+        alert('That is correct! I play brass!')
+    } else {
+        alert('Nope! There is no way I would play an oboe!')
+    }
+
+
+
+
+
+
+
+
+// let age = 17;
+
+// if(age >= 18) {
+//   alert('vote');
+// } else if (age === 19) {
+//   alert('one year with vote')
+// }
+// else {
+//   alert('too young :(')
+// }
+
+// // switch statements
+
+// switch(age) {
+//   case(17):
+//   alert('almost can vote');
+//   break;
+//   case(18):
+//   alert('you now can vote');
+//   break;
+//   case(19):
+//   alert('one year voting');
+//   break;
+//   default:
+//   alert('too young or old enough');
+//   break;
+// }
+
+// alert('mixED Case TEXT'.toLowerCase());
+// alert('mixED Case TEXT'.toUpperCase());
